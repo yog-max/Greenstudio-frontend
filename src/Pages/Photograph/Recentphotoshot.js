5bc2025f1e0c44c72be73dd1dde2d627e5481e1b
Yogeswararao-Dev <yogeshbonigala07@gmail.com>
2026-03-25 20:00:36 +0530

    Refactor Recentphotoshot component and update image paths
    Removed unused location and query parameter logic. Updated image paths to use consistent casing.

--- src/Pages/Photograph/Recentphotoshot.js
+++ src/Pages/Photograph/Recentphotoshot.js
@@ -1,28 +1,21 @@
-import { useLocation } from "react-router-dom";
 import "./Recentphotoshot.css"
 
 function Recentphotoshot()
 {
-     let location=useLocation();
-     console.log(location);
-     const queryParams = new URLSearchParams(location.search);
-     const res = queryParams.get('yogi');
-     console.log(res)
-     const res1=queryParams.get('pavan');
-     console.log(res1+1);
 
-       const images = [ "/Assests/Images/img4.jpg",
-         "/Assests/Images/img12.jpg", 
-         "/Assests/Images/img3.jpg",
-          "/Assests/Images/img9.jpg", 
-          "/Assests/Images/img5.jpg",
-           "/Assests/Images/img6.jpg", 
-           "/Assests/Images/img7.jpg", 
-           "/Assests/Images/img8.jpg", 
-           "/Assests/Images/img1.jpg", 
-           "/Assests/Images/img10.jpg",
-           "/Assests/Images/img11.jpg",
-           "/Assests/Images/img2.jpg"
+       const images = [
+     "/Assests/Images/Img4.jpg",
+         "/Assests/Images/Img12.jpg", 
+         "/Assests/Images/Img3.jpg",
+          "/Assests/Images/Img9.jpg", 
+          "/Assests/Images/Img5.jpg",
+           "/Assests/Images/Img6.jpg", 
+           "/Assests/Images/Img7.jpg", 
+           "/Assests/Images/Img8.jpg", 
+           "/Assests/Images/Img1.jpg", 
+           "/Assests/Images/Img10.jpg",
+           "/Assests/Images/Img11.jpg",
+           "/Assests/Images/Img2.jpg"
          ];
 
    return (
