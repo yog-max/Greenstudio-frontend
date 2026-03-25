89cba447acb4cc968cc9cd5aee48d0cce2fafd90
Yogeswararao-Dev <yogeshbonigala07@gmail.com>
2026-03-25 18:28:58 +0530

    Refactor Recentphotoshot component structure

--- src/Pages/Photograph/Recentphotoshot.js
+++ src/Pages/Photograph/Recentphotoshot.js
@@ -25,19 +25,27 @@ function Recentphotoshot()
            "/Assests/Images/img2.jpg"
          ];
 
-    return(
- <>
+   return (
+  <>
     <h2 className="text-white">Here Green Studio latest clicks</h2>
 
-       <div className="image-grid"> {images.map((src, index) =>
-             ( <div className="image-wrapper" key={index}> 
-             <img src={src} alt={`img-${index}`} 
-             loading="lazy" 
-          className={`grid-image opacity-0 animate-fadeIn`}
+    <div className="image-grid">
+      {images.map((src, index) => (
+        <div className="image-wrapper" key={index}>
+          <img
+            src={src}
+            alt={`img-${index}`}
+            loading="lazy"
+            className="grid-image opacity-0 animate-fadeIn"
             style={{ animationDelay: `${index * 0.3}s` }}
             onLoad={(e) => e.currentTarget.classList.remove("opacity-0")}
-    </>
-    )
+          />
+        </div>
+      ))}
+    </div>
+  </>
+);
+
    
 }
 export default Recentphotoshot;
